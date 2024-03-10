05f5190e3af0a15895fab242ef444dc1d6e8a2dc
Demetrovichz <pugehok322@gmail.com>
2024-03-10 18:52:41 +0300

    fixed icon last project

--- src/components/itemsLink/index.tsx
+++ src/components/itemsLink/index.tsx
@@ -1,10 +1,12 @@
 interface props {
-  image: string;
+  image?: string;
   url: string;
   title: string;
 }
 
+
 export const ItemsLink = (props: props) => {
+  const test = `${props}`
   return (
     <li className="bg-secondary-purple h-14 w-full flex items-center space-x-3 rounded-xl cursor-pointer transition-all duration-150 ease-in hover:bg-main-purple hover:shadow-xl">
       <img className="w-6 h-6 ml-2 xl:w-8 xl:h-8 xl:ml-4" src={props.image} alt={props.title} />
